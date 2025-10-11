6489ad48fc7c0a64fa91f9b51aa6228abff88225
modanas <mdanasjawed2601@gmail.com>
2025-10-11 19:25:08 +0530

    Initial commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,4 @@
-import banner from '../assets/images/banner.png';
+import banner from "../assets/images/Banner.png";
 
 function Banner() {
 	return (
@@ -16,7 +16,7 @@ function Banner() {
 							deserunt possimus, minus, eos doloremque rem facilis sint quasi
 							quis distinctio consequuntur?
 						</p>
-      <label className="input input-bordered flex items-center gap-2">
+						<label className="input input-bordered flex items-center gap-2">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 16 16"
@@ -26,14 +26,22 @@ function Banner() {
 								<path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
 								<path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
 							</svg>
-							<input type="text" className="grow" placeholder="Enter your email to login" />
+							<input
+								type="text"
+								className="grow"
+								placeholder="Enter your email to login"
+							/>
 						</label>
 					</div>
-     <button className="m-6 btn btn-secondary">Get Started</button>
+					<button className="m-6 btn btn-secondary">Get Started</button>
 				</div>
 				<div className="order-1 w-full md:w-1/2">
-    <img src={banner} alt="banner" className="md:w-[500px] md:h-[420px] md:ml-12 md:mt-10"/>
-    </div>
+					<img
+						src={banner}
+						alt="banner"
+						className="md:w-[500px] md:h-[420px] md:ml-12 md:mt-10"
+					/>
+				</div>
 			</div>
 		</>
 	);
